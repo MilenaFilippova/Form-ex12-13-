e5b4ddde176f812272b13f2394b99ea010b8a237
MilenaFilippova <33898241+MilenaFilippova@users.noreply.github.com>
2019-07-02 01:18:29 +0800

    Add files via upload

---
+++ admindel.php
@@ -0,0 +1,10 @@
+<?php 
+spl_autoload_register();
+use App\RegistrationBase;
+$form= new InviteForm;
+$form->del_in_bd();
+
+<form action="admin.php">
+	<p><input type="submit" value="Вернуться к файлам"></p>
+</form>
+?>
