ac99cdd09547ad2b94c83e50f26d352b9f2f1309
MilenaFilippova <33898241+MilenaFilippova@users.noreply.github.com>
2019-07-02 08:32:14 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -3,7 +3,6 @@ include_once   '/classes/InviteForm.php';
 include_once   '/classes/Database.php';
 
 $form= new InviteForm;
-//$db= new Database();
 
 
 include_once   '\templates\admin-request.php'; 
